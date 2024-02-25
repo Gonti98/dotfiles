752fcbe56fe21f0447dc0bcc6cc20c3b7af1da16
Jacek <123518326+Gonti98@users.noreply.github.com>
2024-02-25 01:32:31 +0100

    Update start.sh
    add tmux to script

--- start.sh
+++ start.sh
@@ -4,8 +4,14 @@ sudo apt update && sudo apt upgrade -y
 
 # install git 
 sudo apt install git -y
+
 # install neofetch
 sudo apt install neofetch -y
+
+# install tmux
+sudo apt install tmux -y
+# install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # install docker 
 curl -fsSL https://get.docker.com -o get-docker.sh
